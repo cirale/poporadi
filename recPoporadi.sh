c6e43acc7454a25e61379946dbbca66b8543f601
cirale <7509217+cirale@users.noreply.github.com>
2018-06-30 02:36:45 +0900

    setting for new program

--- recPoporadi.sh
+++ recPoporadi.sh
@@ -1,7 +1,13 @@
 today=$(date "+%Y%m%d")
+programName="ご注文はラジオですか？？～WELCOME【う・さ！】～"
+artist="佐倉綾音 種田梨沙 佐藤聡美 内田真礼"
 
-./rec_radiko.sh QRR 31 . "ご注文はラジオですか？？～チマメ隊のポポロンラジオ～"
-python3 ./setTag.py "ご注文はラジオですか？？～チマメ隊のポポロンラジオ～"
-filename="ご注文はラジオですか？？～チマメ隊のポポロンラジオ～_${today}.mp3"
-directoryId=`gdrive-linux-x64 list --query "mimeType='application/vnd.google-apps.folder' and name='ご注文はラジオですか？？～チマメ隊のポポロンラジオ～'" | awk '!/Id/{print $1}'`
+#./rec_radiko.sh QRR 31 . $programName
+./rec_radiko.sh QRR 1 . $programName
+python3 ./setTag.py $programName $artist
+filename="ご注文はラジオですか？？～WELCOME【う・さ！】～_${today}.mp3"
+directoryId=`gdrive-linux-x64 list --query "mimeType='application/vnd.google-apps.folder' and name='ご注文はラジオですか？？～WELCOME【う・さ！】～'" | awk '!/Id/{print $1}'`
 /usr/local/bin/gdrive-linux-x64 upload $filename -p $directoryId
+if [ $? -eq 0]; then
+    rm $filename
+fi
--- setTag.py
+++ setTag.py
@@ -4,12 +4,12 @@ import sys
 
 prefix = sys.argv[1]
 now = datetime.datetime.now()
+artist = sys.argv[2]
 
 path = '{0}_{1}.mp3'.format(prefix,now.strftime("%Y%m%d"))
 tags = EasyID3(path)
 tags['title'] = prefix
-tags['artist'] = "水瀬いのり　徳井青空　村川梨衣"
+tags['artist'] = artist
 tags['album'] = prefix
-tags['genre'] = "文化放送"
 
 tags.save()
