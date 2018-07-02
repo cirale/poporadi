95610dc4094bb02e6e6d0cf75871a2319ea847d6
cirale <7509217+cirale@users.noreply.github.com>
2018-07-02 21:40:22 +0900

    modfied get dirId

--- recPoporadi.sh
+++ recPoporadi.sh
@@ -5,7 +5,7 @@ artist="佐倉綾音 種田梨沙 佐藤聡美 内田真礼"
 ./rec_radiko.sh QRR 31 . $programName
 python3 ./setTag.py $programName $artist
 filename="ご注文はラジオですか？？～WELCOME【う・さ！】～_${today}.mp3"
-directoryId=`gdrive-linux-x64 list --query "mimeType='application/vnd.google-apps.folder' and name='ご注文はラジオですか？？～WELCOME【う・さ！】～'" | awk '!/Id/{print $1}'`
+directoryId=`/usr/local/bin/gdrive-linux-x64 list --query "mimeType='application/vnd.google-apps.folder' and name='ご注文はラジオですか？？～WELCOME【う・さ！】～'" | awk '!/Id/{print $1}'`
 /usr/local/bin/gdrive-linux-x64 upload $filename -p $directoryId
 if [ $? -eq 0 ]; then
     rm $filename
